f1717ce3a53da377ae844b0cd0af7ed899b74393
Jesko <jesko@huettenhain.net>
2018-05-06 00:00:22 +0200

    refactoring & new option to disable on screen lock

--- kyson.cc
+++ kyson.cc
@@ -1,140 +1,220 @@
 #include "resource.h"
-#include <windows.h>
+
+#include <Windows.h>
 #include <Wtsapi32.h>
+#include <WinUser.h>
 
-#define WM_TRAYICON WM_USER
-const char INTERNAL_CLASS[] = "kyC";
-const char INTERNAL_WINDO[] = "kyW";
-BOOL keep = FALSE;
-BOOL lock = FALSE;
+class Kyson {
 
-void PopUp(HWND wnd) {
-	HMENU pu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MENU));
-	POINT pt;
-	if (pu && GetCursorPos(&pt)) {
-		pu = GetSubMenu(pu, 0);
-		if (keep) CheckMenuItem(pu, ID_KEEP, MF_CHECKED | MF_BYCOMMAND);
-		if (lock) CheckMenuItem(pu, ID_LOCK, MF_CHECKED | MF_BYCOMMAND);
-		if (SetForegroundWindow(wnd)) {
-			TrackPopupMenu(pu, TPM_BOTTOMALIGN | TPM_LEFTBUTTON,
-				pt.x, pt.y, 0, wnd, NULL);
-		}
-	}
-}
+public:
+    Kyson(
+        bool busy,
+        bool busy_when_locked,
+        bool off_when_locked
+    ) : busy(busy),
+        busy_when_locked(busy_when_locked),
+        off_when_locked(off_when_locked)
+    {
+        nf.cbSize = sizeof(nf);
+        nf.uID = 0;
+        module = GetModuleHandle(NULL);
+    }
 
-BOOL SetIcon(HWND wnd, UINT icon) {
-	NOTIFYICONDATA nf;
-	BOOL rV;
-	nf.cbSize = sizeof(nf);
-	nf.hWnd = wnd;
-	nf.uID = 0;
-	nf.uFlags = 0;
-	nf.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(icon));
-	nf.uFlags = NIF_ICON;
+    LRESULT callback(HWND wnd, UINT msg, WPARAM w, LPARAM l) {
+        switch (msg)
+        {
+        case WM_CREATE:
+            break;
+        case WM_WTSSESSION_CHANGE:
+            switch (w) {
+            case WTS_SESSION_LOCK:
+                if (!busy_when_locked && busy)
+                    screenOff();
+                if (off_when_locked)
+                    SendMessage(wnd, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
+                break;
+            case WTS_SESSION_UNLOCK:
+                if (!busy_when_locked && busy)
+                    screenOn();
+                break;
+            }
+            break;
+        case WM_COMMAND:
+            switch (w) {
+            case ID_KEEP_WHEN_LOCKED:
+                busy_when_locked = !busy_when_locked;
+                break;
+            case ID_LOCK:
+                off_when_locked = !off_when_locked;
+                break;
+            case ID_KEEP:
+                toggle();
+                break;
+            case ID_QUIT:
+                DestroyWindow(wnd);
+                break;
+            }
+            break;
+        case WM_USER:
+            switch (l) {
+            case WM_LBUTTONUP:
+                toggle();
+                break;
+            case WM_RBUTTONUP:
+                openMenu();
+                break;
+            }
+            return TRUE;
+        case WM_DESTROY:
+            screenOff();
+            deleteIcon();
+            PostQuitMessage(0);
+            break;
 
-	if (wnd) {
-		rV = Shell_NotifyIcon(NIM_MODIFY, &nf);
-	}
-	else {
-		nf.hWnd = CreateWindow(
-			INTERNAL_CLASS,
-			INTERNAL_WINDO,
-			0, 0, 0, 0, 0,
-			NULL,
-			NULL,
-			GetModuleHandle(NULL),
-			NULL
-		);
-		nf.uID = 0;
-		nf.uFlags |= NIF_MESSAGE;
-		nf.uCallbackMessage = WM_TRAYICON;
-		rV = Shell_NotifyIcon(NIM_ADD, &nf);
-	}
-	if (icon) 
-		DestroyIcon(nf.hIcon);
-	return rV;
-}
+        }
+        return DefWindowProc(wnd, msg, w, l);
+    }
 
-void DeleteIcon(HWND wnd) {
-	NOTIFYICONDATA nf;
-	nf.cbSize = sizeof(NOTIFYICONDATA);
-	nf.hWnd = wnd;
-	nf.uID = 0;
-	nf.uFlags = 0;
-	Shell_NotifyIcon(NIM_DELETE, &nf);
-}
+    static LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM w, LPARAM l)
+    {
+        Kyson *ksn = reinterpret_cast<Kyson*>(GetWindowLongPtr(wnd, GWLP_USERDATA));
+        return ksn 
+            ? ksn->callback(wnd, msg, w, l)
+            : DefWindowProc(wnd, msg, w, l);
+    }
 
-void SwitchKyson(HWND wnd) {
-	if (keep) {
-		SetThreadExecutionState(ES_CONTINUOUS);
-		SetIcon(wnd, IDI_OFF);
-		keep = FALSE;
-	}
-	else {
-		SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
-		SetIcon(wnd, IDI_SCREEN);
-		keep = TRUE;
-	}
-}
+    bool start() {
+        WNDCLASS wc = { 0 };
+        bool success = false;
+        wc.lpfnWndProc = WndProc;
+        wc.lpszClassName = windowClass;
+        if (RegisterClass(&wc)) {
+            nf.hWnd = CreateWindow(
+                windowClass,
+                windowName,
+                0, 0, 0, 0, 0,
+                NULL,
+                NULL,
+                module,
+                NULL
+            );
+            if (nf.hWnd) {
+                WTSRegisterSessionNotification(nf.hWnd, NOTIFY_FOR_THIS_SESSION);
+                SetLastError(0);
+                if (SetWindowLongPtr(nf.hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this))
+                    || !GetLastError())
+                {
+                    nf.uFlags = NIF_MESSAGE;
+                    nf.uCallbackMessage = windowMessage;
+                    if (Shell_NotifyIcon(NIM_ADD, &nf)) {
+                        MSG msg;
+                        success = true;
+                        setIcon();
+                        while (GetMessage(&msg, NULL, 0, 0)) {
+                            TranslateMessage(&msg);
+                            DispatchMessage(&msg);
+                        }
+                    }
+                }
+                WTSUnRegisterSessionNotification(nf.hWnd);
+            }
+        }
+        return false;
+    }
 
-LRESULT CALLBACK WndProc(HWND wnd, UINT msg, WPARAM w, LPARAM l)
-{
-	switch (msg)
-	{
-	case WM_WTSSESSION_CHANGE:
-		if (lock && w == WTS_SESSION_LOCK)
-			SendMessage(wnd, WM_SYSCOMMAND, SC_MONITORPOWER, 2);
-		break;
-	case WM_CREATE:
-		WTSRegisterSessionNotification(wnd, NOTIFY_FOR_THIS_SESSION);
-		break;
-	case WM_COMMAND:
-		switch (w) {
-		case ID_LOCK:
-			lock = !lock;
-			break;
-		case ID_KEEP:
-			SwitchKyson(wnd);
-			break;
-		case ID_QUIT:
-			DestroyWindow(wnd);
-			break;
-		}
-		break;
-	case WM_TRAYICON:
-		switch (l) {
-		case WM_LBUTTONUP:
-			SwitchKyson(wnd);
-			break;
-		case WM_RBUTTONUP:
-			PopUp(wnd);
-			break;
-		}
-		return TRUE;
-	case WM_DESTROY:
-		if (keep) SwitchKyson(wnd);
-		DeleteIcon(wnd);
-		PostQuitMessage(0);
-		break;
+private:
+    void toggle() {
+        busy = !busy;
+        setIcon();
+        if (busy) screenOn();
+        else screenOff();
+    }
 
-	}
-	return DefWindowProc(wnd, msg, w, l);
-}
+    void screenOn() {
+        SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+    }
+    void screenOff() {
+        SetThreadExecutionState(ES_CONTINUOUS);
+    }
+
+    bool setIcon() {
+        nf.uFlags = NIF_ICON;
+        nf.hIcon = LoadIcon(module, MAKEINTRESOURCE(busy ? IDI_SCREEN : IDI_OFF));
+        bool success = !!Shell_NotifyIcon(NIM_MODIFY, &nf);
+        DestroyIcon(nf.hIcon);
+        return success;
+    }
+
+    void deleteIcon() {
+        nf.uFlags = 0;
+        Shell_NotifyIcon(NIM_DELETE, &nf);
+    }
+
+    void openMenu() {
+        HMENU pu = LoadMenu(GetModuleHandle(NULL), MAKEINTRESOURCE(IDR_MENU));
+        POINT pt;
+        if (pu && GetCursorPos(&pt)) {
+            pu = GetSubMenu(pu, 0);
+            if (busy) 
+                CheckMenuItem(pu, ID_KEEP, MF_CHECKED | MF_BYCOMMAND);
+            if (busy_when_locked)
+                CheckMenuItem(pu, ID_KEEP_WHEN_LOCKED, MF_CHECKED | MF_BYCOMMAND);
+            if (off_when_locked) 
+                CheckMenuItem(pu, ID_LOCK, MF_CHECKED | MF_BYCOMMAND);
+
+            if (SetForegroundWindow(nf.hWnd)) {
+                TrackPopupMenu(pu, TPM_BOTTOMALIGN | TPM_LEFTBUTTON,
+                    pt.x, pt.y, 0, nf.hWnd, NULL);
+            }
+        }
+    }
+
+private:
+    const static char windowClass[];
+    const static char windowName[];
+    const static UINT windowMessage = WM_USER;
+
+private:
+    bool busy;
+    bool busy_when_locked;
+    bool off_when_locked;
+    HMODULE module;
+    NOTIFYICONDATA nf;
+};
+
+const char Kyson::windowClass[] = "kyC";
+const char Kyson::windowName[]  = "kyW";
 
 void __declspec(noreturn) main() {
-	WNDCLASS wc = { 0 };
-	wc.lpfnWndProc = WndProc;
-	wc.lpszClassName = INTERNAL_CLASS;
-	if (RegisterClass(&wc)) {
-		keep = FALSE;
-		lock = TRUE;
-		if (SetIcon(NULL, IDI_OFF)) {
-			MSG msg;
-			while (GetMessage(&msg, NULL, 0, 0)) {
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
-		}
-	}
-    ExitProcess(0);
+    int k, argc;
+    bool busy = true;
+    bool busy_while_locked = false;
+    bool off_when_locked = true;
+
+    WCHAR **argv = CommandLineToArgvW(
+        GetCommandLineW(), &argc);
+
+    for (k = 0; k < argc; k++) {
+        /* if this is a one letter flag: */
+        if (argv[k][0] == '/' && argv[k][3] == 0) {
+            switch (argv[k][1]) {
+            case 'B':
+                busy = false;
+                break;
+            case 'L':
+                busy_while_locked = true;
+                break;
+            case '0':
+                off_when_locked = false;
+                break;
+            }
+        }
+    }
+
+    Kyson K(busy, busy_while_locked, off_when_locked);
+
+    if (K.start())
+        ExitProcess(0);
+    else
+        ExitProcess(1);
 }
--- resource.h
+++ resource.h
@@ -16,13 +16,15 @@
 #define ID_KYSON_SCREENOFFWHENLOCKED    40008
 #define ID_SLOCK                        40009
 #define ID_LOCK                         40010
+#define ID_KYSON_KEEPSCREENONWHENLOCKED 40011
+#define ID_KEEP_WHEN_LOCKED             40012
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_COMMAND_VALUE         40013
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
